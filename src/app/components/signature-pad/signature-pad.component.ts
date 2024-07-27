cb216f444c9707a12f5449ca035051f031afa360
Brendo-Perigolo <brendoaperigolo@gmail.com>
2024-07-27 10:53:52 -0300

    Incluso Modulo Assinaturar v10 - teste

--- src/app/components/signature-pad/signature-pad.component.ts
+++ src/app/components/signature-pad/signature-pad.component.ts
@@ -1,4 +1,4 @@
-import { isPlatformBrowser } from "@angular/common";
+import { CommonModule, isPlatformBrowser } from "@angular/common";
 import {
   AfterViewInit,
   Component,
@@ -10,11 +10,12 @@ import {
 } from "@angular/core";
 import SignaturePad from "signature_pad";
 import { PdfServiceService } from "../../services/pdf-service.service";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-signature-pad",
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: "./signature-pad.component.html",
   styleUrl: "./signature-pad.component.scss",
 })
@@ -27,10 +28,13 @@ export class SignaturePadComponent implements AfterViewInit {
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  showPad = true;
+
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
       if (this.signaturePadElement) {
         this.signaturePad = new SignaturePad(this.signaturePadElement.nativeElement);
+        this.drawBaseLine();
       } else {
         console.error("Elemento de assinatura não encontrado.");
       }
@@ -39,6 +43,7 @@ export class SignaturePadComponent implements AfterViewInit {
 
   clear() {
     this.signaturePad?.clear();
+    this.showPad = true;
   }
 
   save() {
@@ -48,5 +53,19 @@ export class SignaturePadComponent implements AfterViewInit {
     }
     this.signatureImage = this.signaturePad.toDataURL("image/png");
     this.pdfService.setSignatureImage(this.signatureImage);
+    this.showPad = false;
+  }
+
+  private drawBaseLine() {
+    const canvas = this.signaturePadElement.nativeElement;
+    const context = canvas.getContext("2d");
+    const lineOffset = 50; // Ajuste este valor conforme necessário
+
+    context.beginPath();
+    context.moveTo(lineOffset, 3); // Início da linha (no deslocamento do topo esquerdo)
+    context.lineTo(lineOffset, canvas.height); // Fim da linha (no deslocamento da parte inferior esquerda)
+    context.strokeStyle = "black"; // Cor da linha
+    context.lineWidth = 1; // Espessura da linha
+    context.stroke();
   }
 }
